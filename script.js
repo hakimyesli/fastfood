f1aa681073581c07195642994c605459953d9141
hakimyesli <hakimyesli2003@gmail.com>
2024-02-09 16:58:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let bars = document.querySelector('.bars');
+let close = document.querySelector('.close');
+let header = document.querySelector('header')
+function open(){
+    header.classList.toggle('open')
+}
+
+bars.addEventListener('click',open)
+close.addEventListener('click',open)
